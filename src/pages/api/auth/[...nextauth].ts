655b4254e7908254ec65b561588a41766ec6fa67
bbruscatto <bruscatto@gmail.com>
2022-04-07 22:46:20 -0300

    feat: rotas | fix:favicon

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import { query as q } from 'faunadb'
 import GithubProvider from "next-auth/providers/github"
+import { useSession, signIn } from 'next-auth/react';
 
 import { fauna } from "../../../services/fauna"
 
@@ -18,6 +19,43 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    session: async ({ session }) => {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        )
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
+
+    },
     async signIn({ user, account, profile }) {
       const { email } = user
 
